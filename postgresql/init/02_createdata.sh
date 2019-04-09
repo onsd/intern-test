7943fa6881a091ee7b01d6adc1069bf007613de2
Takamichi Omori <merrytakamerry@gmail.com>
2019-04-09 22:08:43 +0900

    add directory in order to use postgresql

---
+++ postgresql/init/02_createdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+psql -U postgres -d wantedly << "EOSQL"
+ALTER DATABASE wantedly SET timezone TO 'Asia/Tokyo';
+EOSQL
+
+psql -U postgres -d wantedly << "EOSQL"
+create table users(
+  id SERIAL PRIMARY KEY,
+  name varchar(128),
+  email varchar(128),
+  created_at timestamptz,
+  updated_at timestamptz 
+  )
+EOSQL
+
+psql -U postgres -d wantedly << "EOSQL"
+  insert into users (name, email, created_at, updated_at )values ('Rito', 'rito@example.com', '2019-01-21 12:00:00', '2019-01-21 12:00:00')
+EOSQL
+
+psql -U postgres -d wantedly << "EOSQL"
+  insert into users (name, email, created_at, updated_at )values ('Minami', 'south@example.com', '2019-01-21 12:00:00', '2019-01-21 12:00:00')
+EOSQL
